41342f4495fc5d9bd2dd9a48acb410686edb43a0
Anna Liner <annaliner89@gmail.com>
2023-04-22 16:07:08 -0500

    theme and colors added

---
+++ mizfits_client/src/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+    common: {
+      white: "#fff",
+      black: "#000",
+    },
+
+    pinkRed: "#FEDD69",
+    orange: "#FFAE4F",
+    yellow: "#FEDD69",
+    lightYellow: "#FFE793"
+  }
