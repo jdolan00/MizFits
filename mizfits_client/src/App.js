89c9efb684222ac50d9d1cb7abce5b9c17b6f695
EastAsianInc <113070877+EastAsianInc@users.noreply.github.com>
2023-04-23 19:04:57 -0500

    NavFix

--- mizfits_client/src/App.js
+++ mizfits_client/src/App.js
@@ -1,39 +1,29 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-import HomePage from "scenes/homePage/homePage";
 import LogInPage from "scenes/loginPage/LogInPage";
-import ProfilePage from "scenes/profilePage/ProfilePage";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
 import { useMemo } from "react";
 
+import Navbar from './components/Navbar';
+import HomePage from './scenes/homePage/homePage';
+import LoginPage from './scenes/loginPage/LogInPage';
+import ProfilePage from './scenes/profilePage/ProfilePage';
+import WorkoutPage from './scenes/workoutPage/workoutPage';
 
-function App() {
-  const mode = useSelector((state) => state.mode);
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-  const isAuth = Boolean(useSelector((state) => state.token));
-
+const App = () => {
   return (
-    <div className="app">
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Routes>
-            <Route path="/" element={<LogInPage />} />
-            <Route
-              path="/home"
-              element={isAuth ? <HomePage /> : <Navigate to="/" />}
-            />
-            <Route
-              path="/profile/:userId"
-              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
-            />
-          </Routes>
-        </ThemeProvider>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/workout" element={<WorkoutPage />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ mizfits_client/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/workout">Workout</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
