3a497ee5160fac559f9bb950ac9bd225c8a89f18
EastAsianInc <113070877+EastAsianInc@users.noreply.github.com>
2023-04-25 23:18:32 -0500

    Aggregation Functional and presentable(ish)

--- mizfits_client/src/App.js
+++ mizfits_client/src/App.js
@@ -1,17 +1,12 @@
 import { BrowserRouter, /*Navigate,*/ Routes, Route } from "react-router-dom";
-// import LogInPage from "scenes/loginPage/LogInPage";
-// import { useSelector } from "react-redux";
-// import { CssBaseline, ThemeProvider } from "@mui/material";
-// import { createTheme } from "@mui/material/styles";
-// import { themeSettings } from "./theme";
-// import { useMemo } from "react";
-
 import Navbar from './components/Navbar';
 import HomePage from './scenes/homePage/homePage';
 import LoginPage from './scenes/loginPage/LogInPage';
 import ProfilePage from './scenes/profilePage/ProfilePage';
 import WorkoutPage from './scenes/workoutPage/workoutPage';
 import WorkoutPlans from './scenes/workoutPage/Workoutplans';
+import Workouts from './scenes/Workouts/Workouts';
+import Workout from './scenes/Workouts/Workout';
 
 const App = () => {
   return (
@@ -23,6 +18,8 @@ const App = () => {
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/workout" element={<WorkoutPage />} />
         <Route path="/routine" element={<WorkoutPlans />} />
+        <Route path="/workouts" element={<Workouts />} />
+        <Route path="/workouts/:workoutName" element={<Workout />} />
       </Routes>
     </BrowserRouter>
   );
