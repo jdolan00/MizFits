d6c1d7ed71fea767fd2667a39657acd8eeaed053
Winston104 <54595899+Winston104@users.noreply.github.com>
2023-05-02 12:48:39 -0500

    Update homePage.jsx

--- mizfits_client/src/scenes/homePage/homePage.jsx
+++ mizfits_client/src/scenes/homePage/homePage.jsx
@@ -4,30 +4,50 @@ import {
   Box,
   Typography,
   Container,
+  Grid,
+  Avatar,
 } from "@mui/material";
-import {ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import { Footer } from "components/Footer";
-import {theme} from "theme"
+import { theme } from "theme";
+// import your own image here
+const avatarImage =
+  "https://images.pexels.com/photos/841130/pexels-photo-841130.jpeg?cs=srgb&dl=pexels-victor-freitas-841130.jpg&fm=jpg";
 
-
-export default function HomePage() {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <main>
-        <Box>
-          <Container maxWidth="sm">
-            <Typography component="h1" variant="h2" align="center" gutterBottom>
-              Welcome!
-            </Typography>
-            <Typography variant="h5" align="center" paragraph>
-              Start tracking your activity today.
-            </Typography>
-          </Container>
-        </Box>
-        <Container maxWidth="md">{/* End hero unit */}</Container>
-      </main>
-      <Footer></Footer>
-    </ThemeProvider>
-  );
-}
+  export default function HomePage() {
+    return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <main>
+          <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+            <Grid container spacing={2} alignItems="center">
+              <Grid item>
+                <Container maxWidth="sm">
+                  <Typography
+                    component="h1"
+                    variant="h2"
+                    align="left"
+                    gutterBottom
+                  >
+                    Welcome!
+                  </Typography>
+                  <Typography variant="h5" align="left" paragraph>
+                    Start tracking your activity today.
+                  </Typography>
+                </Container>
+              </Grid>
+              <Grid item>
+                <Avatar
+                  alt="Avatar"
+                  src={avatarImage}
+                  sx={{ width: 200, height: 200 }}
+                />
+              </Grid>
+            </Grid>
+          </Box>
+          <Container maxWidth="md">{/* End hero unit */}</Container>
+        </main>
+        <Footer></Footer>
+      </ThemeProvider>
+    );
+  }
