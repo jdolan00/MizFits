519c238cc0397b98bb1a9f5b47abbd8ec8568b55
Winston104 <54595899+Winston104@users.noreply.github.com>
2023-05-04 20:47:21 -0500

    Update TrackInfo.jsx

--- mizfits_client/src/components/TrackInfo.jsx
+++ mizfits_client/src/components/TrackInfo.jsx
@@ -48,33 +48,3 @@ function TrackInfo(props) {
 }
 
 export default TrackInfo;
-
-// import React from 'react';
-
-// function TrackInfo(props) {
-//     const { title, type, time, distance, sets, reps, weight, description, date } = props.track;
-
-//     return (
-//       <div className="track-info">
-//         <h2>{title}</h2>
-//         <p>Type: {type}</p>
-//         <p>Time: {time}</p>
-//         {type === 'cardio' && (
-//           <p>Distance: {distance}</p>
-//         )}
-//         {type === 'weights' && (
-//           <>
-//             <p>Sets: {sets}</p>
-//             <p>Reps: {reps}</p>
-//             <p>Weight: {weight}</p>
-//           </>
-//         )}
-//         {type === 'create' && (
-//           <p>Description: {description}</p>
-//         )}
-//         {/* <p>Date: {date}</p>  NOT NEEDED FOR USERS SAKE */}
-//       </div>
-//     );
-//   }
-
-//   export default TrackInfo;
